adf14c86bfa0596db0f75d1aaac4ad07b5624c36
Maxim Chistol <maxim.chistol@iis.utm.md>
2025-05-13 12:32:42 +0300

    prima ora de NodeJS

---
+++ nodeJS/index.js
@@ -0,0 +1,26 @@
+// const http = require('http');
+// //am iportat modelul de http
+
+// //creem serverul pentru lucru cu el pe beckend
+// const server = http.createServer((req, res)=> {
+//     res.statisCode = 200;
+//     res.serHeader('Content-Type', 'text/plain');
+//     rex.end('welocme to node JS server');
+
+// });
+
+// //acesarea prin portul 3000 va fi auzit serverul prin acest port
+// server.listen(3000,()=> {
+//     console.log('Server is running on port 3000');
+
+// });
+
+// const fs = require('fs');
+
+// fs.readFile(file.txt, 'utf8', (err, data) => {
+//     if (err) throw err; {
+//         console.log(data);
+//     }
+// });
+
+// console.log('Reading file...');
